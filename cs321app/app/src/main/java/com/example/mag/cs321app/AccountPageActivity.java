9490e44aca43d3eaed8c2b88522a14d2781c6691
Steven <jsbs5563@gmail.com>
2017-04-25 18:32:00 -0400

    Added AccountPageActivity class. -Steven

---
+++ cs321app/app/src/main/java/com/example/mag/cs321app/AccountPageActivity.java
@@ -0,0 +1,26 @@
+package com.example.mag.cs321app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by smashking on 4/25/17.
+ */
+
+public class AccountPageActivity extends AppCompatActivity {
+    private String name;
+    private String gender;
+    private int points;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+}
