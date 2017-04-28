6d944a6039f65e8ed1b2e519785f705ad561cdf8
maggieli8567 <maggieli8567@gmail.com>
2017-04-27 23:10:28 -0400

    set up expresso and include instrumented test cases

--- cs321app/app/src/androidTest/java/com/example/mag/cs321app/ExampleInstrumentedTest.java
+++ cs321app/app/src/androidTest/java/com/example/mag/cs321app/ExampleInstrumentedTest.java
@@ -3,9 +3,18 @@ package com.example.mag.cs321app;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import android.support.test.rule.ActivityTestRule;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+//import static android.support.test.espresso.matcher.ViewMatchers.withText;
+//import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.action.ViewActions.*;
+
 
 import static org.junit.Assert.*;
 
@@ -23,4 +32,32 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.mag.cs321app", appContext.getPackageName());
     }
-}
+
+    private String mStringToBetyped;
+
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        mStringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.emailField))
+                .perform(typeText(mStringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.emailField)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.emailField)) .check(matches(withText(mStringToBetyped)));
+    }
+
+
+
+    }
--- cs321app/app/src/test/java/com/example/mag/cs321app/ExampleUnitTest.java
+++ cs321app/app/src/test/java/com/example/mag/cs321app/ExampleUnitTest.java
@@ -1,8 +1,15 @@
 package com.example.mag.cs321app;
 
+import junit.extensions.ActiveTestSuite;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 
 import static org.junit.Assert.*;
+
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +17,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+
 }
